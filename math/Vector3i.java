9491901be997bc6f89d8bd3fe31fe11c82492bb2
GryphGryph <38794189+GryphGryph@users.noreply.github.com>
2018-07-24 22:43:15 +0200

    Add files via upload

---
+++ math/Vector3i.java
@@ -0,0 +1,15 @@
+package com.pipon.mergeemall.math;
+
+/**
+ * Created by quentin on 11/23/17.
+ */
+
+public class Vector3i {
+    public int x, y, z;
+
+    public Vector3i(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
